7250b80a05eb1cad9e8de05f5a23e4391158f293
remchaf <70279203+remchaf@users.noreply.github.com>
2022-07-23 20:30:20 +0000

    ;

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,6 @@
 def add_time(startTime, endTime, starting_day=''):
     week = ("Monday", "Tuesday", "Wednesday",
-            "Thursday", "Friday", "Saterday", "Sunday")
+            "Thursday", "Friday", "Saturday", "Sunday")
     [h0, m0] = startTime.split(' ')[0].split(':')
     time0 = (int(h0) % 12) * 60 + int(m0)
     if startTime.split(' ')[1] == 'PM':
@@ -50,20 +50,20 @@ def add_time(startTime, endTime, starting_day=''):
     print(string)
 
 
-add_time("3:00 PM", "3:10")
+add_time("2:59 AM", "24:00", "saturDay")
 # Returns: 6:10 PM
 
-add_time("11:30 AM", "2:32", "Monday")
-# Returns: 2:02 PM, Monday
+# add_time("11:30 AM", "2:32", "Monday")
+# # Returns: 2:02 PM, Monday
 
-add_time("11:43 AM", "00:20")
-# Returns: 12:03 PM
+# add_time("11:43 AM", "00:20")
+# # Returns: 12:03 PM
 
-add_time("10:10 PM", "3:30")
-# Returns: 1:40 AM (next day)
+# add_time("10:10 PM", "3:30")
+# # Returns: 1:40 AM (next day)
 
-add_time("11:43 PM", "24:20", "tueSday")
-# Returns: 12:03 AM, Thursday (2 days later)
+# add_time("11:43 PM", "24:20", "tueSday")
+# # Returns: 12:03 AM, Thursday (2 days later)
 
-add_time("6:30 PM", "205:12")
-# Returns: 7:42 AM (9 days later)
+# add_time("6:30 PM", "205:12")
+# # Returns: 7:42 AM (9 days later)
